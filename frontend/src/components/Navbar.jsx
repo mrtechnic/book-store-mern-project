6b0a5cd79eea6e58e293fa33c5124f1ef93a57c7
mrtechnic <osita.ezikeugo22@gmail.com>
2025-01-27 23:51:11 +0100

    setting up the navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='max-w-screen-2xl mx-auto px-4 py-6'>
+      <nav className='flex justify-between items-center'>
+        {/* left side */}
+        <div>logo & search bar</div>
+
+        {/* right side */}
+        <div>
+          nav items 
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
